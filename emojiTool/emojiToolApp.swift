f5c51a7268fdf8790bd7a650bad27f5c502efdb8
Reona Monga <271916571+pokkareonareal-bot@users.noreply.github.com>
2026-03-26 17:32:47 +0900

    Initial Commit

---
+++ emojiTool/emojiToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojiToolApp.swift
+//  emojiTool
+//
+//  Created by Reona Monga on 2026/03/26.
+//
+
+import SwiftUI
+
+@main
+struct emojiToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
